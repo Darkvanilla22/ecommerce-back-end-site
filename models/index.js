fce2446e86239fef9b941d916bf5ba5ddbdef113
Finian Corbett <finian97@gmail.com>
2024-05-30 21:06:46 -0400

    Added several 'onDelete' parameters to index.js

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { on } = require('nodemon');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
@@ -13,18 +14,21 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
+  onDelete: 'CASCADE',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
 });
 
 module.exports = {
